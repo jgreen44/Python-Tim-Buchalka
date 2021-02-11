de32fcf4ae3a2ec3587e4f80c1e216a8cdd4c191
jgreen44 <jgreen44@asu.edu>
2021-02-11 10:20:25 -0600

    accounts.py update

--- Section 10 - OOP/accounts.py
+++ Section 10 - OOP/accounts.py
@@ -1,15 +1,20 @@
+import datetime
+import pytz
+
 class Account:
-    """Siimple account class with balance"""
+    """Simple account class with balance"""
 
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = []
         print("Account created for " + self.name)
 
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
             self.show_balance()
+            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
 
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
@@ -20,6 +25,14 @@ class Account:
 
     def show_balance(self):
         print('Balance is {}'.format(self.balance))
+
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                tran_type = 'deposited'
+            else:
+                tran_type = 'withdrawn'
+            print("{:6} {} on {} (localtime was {})".format(amount, tran_type, date, date.astimezone()))
 
 
 if __name__ == '__main__':
@@ -32,3 +45,5 @@ if __name__ == '__main__':
     # jason.show_balance()
 
     jason.withdraw(2000)
+
+    jason.show_transactions()
