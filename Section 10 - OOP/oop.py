b1482c3b2e6a8ec19c0ba39fd857d00565b0600f
jgreen44 <jgreen44@asu.edu>
2021-08-07 20:06:41 -0500

    update

--- Section 10 - OOP/oop.py
+++ Section 10 - OOP/oop.py
@@ -1,6 +1,6 @@
 class Kettle(object):
 
-    power_source = 'electricity'
+    power_source = "electricity"
 
     def __init__(self, make, price):
         self.make = make
@@ -11,39 +11,48 @@ class Kettle(object):
         self.on = True
 
 
-kenwwood = Kettle('Kenwood', 8.99)
-print(kenwwood.make)
-print(kenwwood.price)
+kenwood = Kettle("Kenwood", 8.99)
+print(kenwood.make)
+print(kenwood.price)
 
-kenwwood.price = 12.75
-print(kenwwood.price)
+kenwood.price = 12.75
+print(kenwood.price)
 
 hamilton = Kettle("Hamilton", 14.55)
 
-print("Models: {} = {}, {} = {}".format(kenwwood.make, kenwwood.price, hamilton.make, hamilton.price))
+print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
 
-print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwwood, hamilton))
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+"""
+Class: template for creating objects. All objects created using the same class will have the same characteristics.
+Object: an instance of a class.
+Instantiate: create an instance of a class.
+Method: a function defined in a class.
+Attribute: a variable bound to an instance of a class.
+"""
 
 print(hamilton.on)
 hamilton.switch_on()
 print(hamilton.on)
 
-Kettle.switch_on(kenwwood)
-print(kenwwood.on)
+Kettle.switch_on(kenwood)
+print(kenwood.on)
+kenwood.switch_on()
 
+print("*" * 80)
 
-print('*' * 80)
-kenwwood.power = 1.5
-print(kenwwood.power)
+kenwood.power = 1.5
+print (kenwood.power)
 # print(hamilton.power)
 
-print(Kettle.power_source)
-kenwwood.power_source = 'gas'
-print(kenwwood.power_source)
-print(hamilton.power_source)
-
 print("Switch to atomic power")
-Kettle.power_source = 'atomic'
+Kettle.power_source = "atomic"
+print(Kettle.power_source)
+print("Switch kenwood to gas")
+kenwood.power_source = "gas"
+print(kenwood.power_source)
+print(hamilton.power_source)
 print(Kettle.__dict__)
-print(kenwwood.__dict__)
+print(kenwood.__dict__)
 print(hamilton.__dict__)
